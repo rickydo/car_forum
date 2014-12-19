543af9d9a30454cd87b48c0b13a95d87cf9baf57
rickydo <g3tdo@yahoo.com>
2014-12-19 16:09:16 -0600

    added password digest and admin user

--- db/migrate/20141219202737_create_users.rb
+++ db/migrate/20141219202737_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
     	t.string 		:username
     	t.string		:first_name
     	t.string		:last_name
+      t.string    :password_digest
     	t.string		:city
     	t.string		:state
     	t.string		:email
