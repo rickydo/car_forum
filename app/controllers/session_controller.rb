7c9146c230eb5ce8664c6ebd75a4f94362421532
rickydo <g3tdo@yahoo.com>
2014-12-22 18:34:44 -0600

    added sessions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,28 @@
 class SessionController < ApplicationController
-	def new
+	def new 
+		if session[:user_id]
+			redirect_to root_path
+		end
+	end
 
+	def create
+		@user = User.find_by(username: params[:username])
+
+		if @user
+			if @user.authenticate(params[:password])
+				session[:user_id] = @user.id
+				redirect_to :back
+			else 
+				redirect_to root_path
+			end
+		else
+			flash[:error] = "Incorrect Username or password"
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_path
 	end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,17 @@
 module SessionHelper
+
+	def sign_in(user)
+		session[:user_id] = user.id
+		current_user = user
+	end
+
+	def current_user
+		User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def sign_out
+		session.delete(:user_id)
+		current_user = nil
+	end
+
 end
